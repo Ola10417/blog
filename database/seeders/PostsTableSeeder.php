04fbdd5a3b259c330dc5cb3961b4d627c7b66d66
Ola M <Ola10417@Gmail.com>
2021-01-01 20:19:24 +0100

    Initial commit

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Post::truncate();
+
+        $faker=\Faker\Factory::create();
+        for ($i = 0; $i < 15; $i++) {
+            \App\Models\Post::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
